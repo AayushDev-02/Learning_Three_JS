1cfa2d4f858e34d4a2de0dbcd2403a18869ac8a1
Aayush Yadav <yadavaayush002@gmail.com>
2023-11-03 00:43:54 +0530

    Added event changes and animations

--- three-js/src/components/Sphere.jsx
+++ three-js/src/components/Sphere.jsx
@@ -1,25 +1,43 @@
 /* eslint-disable react/prop-types */
 
-import { useFrame } from "@react-three/fiber"
-import { useRef } from "react"
+import { events, useFrame } from "@react-three/fiber"
+import { useRef, useState } from "react"
 
 
 
-const Sphere = ({position, color, size}) => {
+const Sphere = ({ position, color, size }) => {
   const ref = useRef()
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
   useFrame((state, delta) => {
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += delta
+    // ref.current.rotation.x += delta
     
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
+    ref.current.rotation.x += delta
+    if(isHovered){
+      ref.current.rotation.y += delta 
+      ref.current.rotation.x += delta
+    }
+
+
+
+
+    // ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
 
   })
   return (
-    <mesh position={position} ref={ref} >    
-        <sphereGeometry args={size} />
-        
-        <meshStandardMaterial color={color} wireframe />
-      </mesh>
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+    >
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={isHovered ? "red" : color} wireframe />
+    </mesh>
   )
 }
 
